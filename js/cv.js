d0f8e419361ec3a7b7bfb26790e62285f7a17614
clesiomatias <clesiofmatias@gmail.com>
2022-09-06 18:46:39 -0300

    conclusão de curriculo pt-br

--- js/cv.js
+++ js/cv.js
@@ -41,3 +41,67 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  if (this.scrollY >= 200) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
+
+// Mudança de tema
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+function scaleCV() {
+  document.body.classList.add("scale-cv");
+}
+function removeScale() {
+  document.body.classList.remove("scale-cv");
+}
+
+// Gerando PDF
+let areaCV = document.getElementById("area-cv");
+let resumeButton = document.getElementById("resume-button");
+let opt = {
+  margin: 0,
+  filename: "Curriculo_Clesio_Matias.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
+
+function geraCurriculo() {
+  html2pdf(areaCV, opt);
+}
+resumeButton.addEventListener("click", () => {
+  scaleCV();
+  geraCurriculo();
+  setTimeout(removeScale, 5000);
+});
